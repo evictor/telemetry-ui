5e3c8ec08aac6b6fde5edc89c2621c0a02b072f0
Ezekiel Victor <zekevictor@gmail.com>
2021-07-18 18:41:17 -0700

    gui scaffolding

---
+++ src/components/Dash.vue
@@ -0,0 +1,33 @@
+<template>
+  <div>
+    <h1>evictor/telemetry-ui</h1>
+    <h2>UI portion of Loop Telemetry Test</h2>
+
+
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Dash',
+  props: {
+  }
+}
+</script>
+
+<style scoped>
+h3 {
+  margin: 40px 0 0;
+}
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+a {
+  color: #42b983;
+}
+</style>
